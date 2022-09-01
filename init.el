df39747db71f33277c393f7030e50ae8606cee43
Tobi Okanlawon <tobiinlondon34@gmail.com>
2022-09-01 08:35:07 +0100

    Add initial init.el file

---
+++ init.el
@@ -0,0 +1,58 @@
+;; start the initial frame maximized
+(add-to-list 'initial-frame-alist '(fullscreen . maximized))
+
+;; start every frame maximized
+(add-to-list 'default-frame-alist '(fullscreen . maximized))
+
+;; disable menu bar on startup
+(menu-bar-mode -1)
+
+;; disable tool bar on startup
+(tool-bar-mode -1)
+
+;; disable scroll bar on startup
+(toggle-scroll-bar -1)
+
+;; inhibit splash screen
+(setq inhibit-splash-screen t)
+
+;; line numbers
+(global-display-line-numbers-mode t)
+(setq display-line-numbers-type 'absolute)
+
+;; set up MELPA
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages '(dracula-theme)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+;; load dracula theme
+(load-theme 'dracula t)
+
+;; beancount mode for plaintext bookkeeping.
+(add-to-list 'load-path "~/Downloads/beancount-mode/")
+(require 'beancount)
+(add-to-list 'auto-mode-alist '("\\.beancount\\'" . beancount-mode))
+
+;; disable auto-indent for beancount-mode
+(add-hook 'beancount-mode-hook
+	  (lambda () (setq-local electric-indent-chars nil)))
+
+;; use outline mode for beancount. To allow subsectioning.
+(add-hook 'beancount-mode-hook #'outline-minor-mode)
+
+;; change keyboard mappings for outline minor mode when used with beancount major mode.
+(define-key beancount-mode-map (kbd "C-c C-n") #'outline-next-visible-heading)
+(define-key beancount-mode-map (kbd "C-c C-p") #'outline-previous-visible-heading)
