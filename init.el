010e4c83f274744248b55f08dd04e909387329a4
Tobi Okanlawon <tobiinlondon34@gmail.com>
2022-10-09 21:18:28 +0100

    Add helm-dash and time mode to mode line

--- init.el
+++ init.el
@@ -30,7 +30,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(emmet-mode dracula-theme)))
+ '(package-selected-packages '(helm-dash emmet-mode dracula-theme)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -78,3 +78,7 @@
 (define-key global-map "\C-csl" 'org-store-link)
 (define-key global-map "\C-ca" 'org-agenda)
 (setq org-log-done t)
+
+
+;; enable the time in mode-line
+(display-time-mode 1)
